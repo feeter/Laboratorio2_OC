9915f86dbfa357e86e939c3d4a158f22297818a4
Jose Campos <josigna.cp@gmail.com>
2019-12-24 19:10:22 -0300

    se imprime en un archivo de texto la rutas de las carpeta dada por parámetro el método

--- Laboratorio_2_OC/main.c
+++ Laboratorio_2_OC/main.c
@@ -9,46 +9,91 @@
 //  Copyright © 2019 Jose Ignacio Campos Padilla. All rights reserved.
 //
 
+
+
 #include <unistd.h>
 #include <sys/types.h>
 #include <dirent.h>
 #include <stdio.h>
-#include <string.h>
 
 #define _POSIX_SOURCE
 #include <sys/types.h>
 #include <pwd.h>
 #include <unistd.h>
 
+#define _GNU_SOURCE         /* See feature_test_macros(7) */
+#include <string.h>
 
-void listdir(const char *name, int indent)
+char *gnu_basename(char *path)
 {
-    DIR *dir;
-    struct dirent *entry;
+    char *base = strrchr(path, '/');
+    return base ? base+1 : path;
+}
 
-    if (!(dir = opendir(name)))
+
+void listdir(const char *name, int margen, FILE* fp)
+{
+    DIR* dirActual;
+    struct dirent *entry;
+    
+
+    if (!(dirActual = opendir(name)))
         return;
 
-    while ((entry = readdir(dir)) != NULL) {
+    while ((entry = readdir(dirActual)) != NULL)
+    {
         
-        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0 || strcmp(entry->d_name, ".DS_Store") == 0)
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0 || entry->d_type != DT_DIR)//strcmp(entry->d_name, ".DS_Store") == 0)
             continue;
+        
         
         if (entry->d_type == DT_DIR) {
             char path[1024];
             
-            snprintf(path, sizeof(path), "%s/%s", name, entry->d_name);
-            printf("%*s[%s]\n", indent, "", entry->d_name);
-            listdir(path, indent + 2);
-        } else {
+            snprintf(path, sizeof(path), "%s/%s", name, entry->d_name); // concatena la ruta completa
+            printf("%*s%s\n", margen, "", entry->d_name); // imprime el nombre de la carpeta actual por consola
+            fprintf(fp, "%*s%s\n", margen, "", entry->d_name); // escribe en el archivo fp el nombre de la carpeta actual
             
-            printf("%*s- %s\n", indent, "", entry->d_name);
+
+            //listdir(path, margen + 2, fp);
+            listdir(path, 0, fp);
+            
+            char *basec, *bname;
+            
+
+            basec = strdup(name);
+            bname = gnu_basename(basec);
+            
+            printf("%s\n", bname);
+            fprintf(fp, "%s\n", bname); // escribe en el archivo fp el nombre de la carpeta actual
+            
+            
+        } else {
+            // Archivos
+            printf("%*s- %s\n", margen, "", entry->d_name);
+            fprintf(fp, "%*s- %s\n", margen, "", entry->d_name);
         }
     }
-    closedir(dir);
+    
+    closedir(dirActual);
 }
 
-void getUserId()
+FILE* CrearArchivo(char* ubicacion)
+{
+    
+    FILE* fp = fopen(ubicacion,"w");
+    
+    
+    return fp;
+    
+}
+
+void CerrarArchivo(FILE* fp)
+{
+    fclose(fp);
+}
+
+void getUserId(FILE* fp)
 {
     struct passwd *p;
     uid_t  uid;
@@ -59,10 +104,15 @@ void getUserId()
       //puts("getpwuid() returned the following info for your userid:");
       //printf("  pw_name  : %s\n",       p->pw_name);
       printf("Id_usuario: %d ", (int) p->pw_uid);
+        fprintf (fp, "Id_usuario: %d ", (int) p->pw_uid);
       //printf("  pw_gid   : %d\n", (int) p->pw_gid);
       //printf("  pw_dir   : %s\n",       p->pw_dir);
       //printf("  pw_shell : %s\n",       p->pw_shell);
       printf("Id_programa: %d\n", (int)   getpid());
+        
+        
+        fprintf (fp, "Id_programa: %d\n", (int)   getpid());
+
     }
     
     printf("\n");
@@ -70,12 +120,16 @@ void getUserId()
 }
 
 
+
+
 int main(int argc, const char * argv[]) {
-    getUserId();
     
+    FILE* fp = CrearArchivo("/Users/josigna.cp/Documents/USACH/Materias/Semestre 2/ORGANIZACIÓN DE COMPUTADORES/Recorrido.txt");
     
-    listdir("/Users/josigna.cp/Documents/USACH/Materias/Semestre 2/ORGANIZACIÓN DE COMPUTADORES/Laboratorio 2/Inicio/", 0);
+    getUserId(fp);
+    listdir("/Users/josigna.cp/Documents/USACH/Materias/Semestre 2/ORGANIZACIÓN DE COMPUTADORES/Laboratorio 2/", 0, fp);
     
+    CerrarArchivo(fp);
     
     
 
